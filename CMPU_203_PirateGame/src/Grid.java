3f4cc7d6beff3965e5827e9f7bf23d85202fccc1
Erin Mutchek <emutchek@mail.cs.vassar.edu>
2023-02-22 20:25:40 -0500

    edited grid class

--- CMPU_203_PirateGame/src/Grid.java
+++ CMPU_203_PirateGame/src/Grid.java
@@ -5,63 +5,72 @@
  */
 public class Grid {
     int ship_location; //0-50
-    ASurrounding [] all_surroundings; //all islands & resource areas on world map
-    String [][] g = {
-            {all_surroundings[0].toString(), " "},
-            {" ", " "},
-            {" ", all_surroundings[1].toString()},
-            {all_surroundings[2].toString(), " "},
-            {all_surroundings[3].toString(), " "},
-            {" ", all_surroundings[4].toString()},
-            {" ", " "},
-            {" ", " "},
-            {all_surroundings[5].toString(), " "},
-            {" ", all_surroundings[6].toString()},
-            {" ", " "},
-            {" ", all_surroundings[7].toString()},
-            {all_surroundings[8].toString(), " "},
-            {" ", " "},
-            {all_surroundings[9].toString(), " "},
-            {all_surroundings[10].toString(), " "},
-            {" ", " "},
-            {" ", " "},
-            {" ", " "},
-            {all_surroundings[11].toString(), " "},
-            {" ", all_surroundings[12].toString()},
-            {" ", " "},
-            {" ", all_surroundings[13].toString()},
-            {" ", " "},
-            {" ", all_surroundings[14].toString()},
-            {" ", " "},
-            {all_surroundings[15].toString(), " "},
-            {" ", all_surroundings[16].toString()},
-            {" ", " "},
-            {" ", all_surroundings[17].toString()},
-            {" ", " "},
-            {" ", all_surroundings[18].toString()},
-            {all_surroundings[19].toString(), " "},
-            {" ", " "},
-            {all_surroundings[20].toString(), " "},
-    };
+    ASurrounding [] all_surroundings = new ASurrounding[21]; //all islands & resource areas on world map
+    String [][] grid;
 
     public Grid() {
-        ASurrounding a = new Island();
-        ASurrounding b = new Island();
-        all_surroundings = new ASurrounding[]{a,b};
+        for(int i = 0; i < 21; i++) {
+            all_surroundings [i] = new Island();
+        }
+        grid = new String [][]{
+        {all_surroundings[0].toString(), " "},
+        {" ", " "},
+        {" ", all_surroundings[1].toString()},
+        {all_surroundings[2].toString(), " "},
+        {all_surroundings[3].toString(), " "},
+        {" ", all_surroundings[4].toString()},
+        {" ", " "},
+        {" ", " "},
+        {all_surroundings[5].toString(), " "},
+        {" ", all_surroundings[6].toString()},
+        {" ", " "},
+        {" ", all_surroundings[7].toString()},
+        {all_surroundings[8].toString(), " "},
+        {" ", " "},
+        {all_surroundings[9].toString(), " "},
+        {all_surroundings[10].toString(), " "},
+        {" ", " "},
+        {" ", " "},
+        {" ", " "},
+        {all_surroundings[11].toString(), " "},
+        {" ", all_surroundings[12].toString()},
+        {" ", " "},
+        {" ", all_surroundings[13].toString()},
+        {" ", " "},
+        {" ", all_surroundings[14].toString()},
+        {" ", " "},
+        {all_surroundings[15].toString(), " "},
+        {" ", all_surroundings[16].toString()},
+        {" ", " "},
+        {" ", all_surroundings[17].toString()},
+        {" ", " "},
+        {" ", all_surroundings[18].toString()},
+        {all_surroundings[19].toString(), " "},
+        {" ", " "},
+        {all_surroundings[20].toString(), " "}};
     }
-    //needs to be fixed
+
     public String toString() {
-        return null;
+        String s = "";
+        for(int r = this.ship_location + 4; r < 5; r++) {
+                s+= "|" + this.grid[r][0] + "| |" + this.grid[r][1] + "|\n";
+        }
+        return s;
     }
+
     public void populateGrid() {
-        for (int r=ship_location + 4; r >= ship_location; r--){
+        for (int r=this.ship_location + 4; r >= this.ship_location; r--){
             for (int c=0; c < 2; c++) {
-                if(!(g[r][c].equals("O"))) {//if this element isn't an island
-                    g[r][c] = "-"; //put something there
+                if(!(this.grid[r][c].equals("O"))) {//if this element isn't an island
+                    this.grid[r][c] = "-"; //put something there
                 }
             }
         }
     }
-    public void move(){}
+    public void move(){
+        this.ship_location++;
+        populateGrid();
+        System.out.print(this);
+    }
     public void identifyIslands() {}
 }
