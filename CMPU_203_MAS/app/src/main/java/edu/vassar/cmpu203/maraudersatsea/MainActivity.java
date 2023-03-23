cd63e4521278fc21cfd4b53396e7997e418c128f
phuded <phuded@vassar.edu>
2023-03-22 20:37:56 -0400

    Added NEW android studio file

---
+++ CMPU_203_MAS/app/src/main/java/edu/vassar/cmpu203/maraudersatsea/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.vassar.cmpu203.maraudersatsea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
