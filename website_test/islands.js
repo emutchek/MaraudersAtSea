f6a4d0138c52a199b8a2452d183c68e7201e5d35
emutchek <erin@mutchek.com>
2023-07-22 11:30:44 -0400

    totally works except i forgot there was island 0

--- website_test/islands.js
+++ website_test/islands.js
@@ -21,7 +21,7 @@ class Tree {
 		if (this.head == null) { // edge case: inserting first node
 			this.head = node;
 		}
-		else if (attachTo == '' && tag.slice(-1)=='b') { // adding onto end of tree (right side)
+		else if (attachTo == '' && node.tag.slice(-1)=='b') { // adding onto end of tree (right side)
 			this.addToEnd(false,node);
 		}
 		else if (attachTo == '') { // adding onto end of tree (left side)
@@ -37,14 +37,13 @@ class Tree {
 		}
 		this.size++;
 	}
-	
 	getNextIsland(aOrDefault = true) { // moves pointer, then returns value
-		if (this.curIsland.tag == '9') aOrDefault = earnedGoodEnding();
-		// return very first island
-		if (this.curIsland == null) this.curIsland = this.head;
-		// either there's no branch, or they're going to 'a' version
-		else if (aOrDefault) this.curIsland = this.curIsland.baseLeft;
-		else this.curIsland = this.curIsland.baseRight; // going to 'b' version
+		if (this.curIsland == null) this.curIsland = this.head; // return very first island
+		else {
+			if (this.curIsland.tag == '9') aOrDefault = earnedGoodEnding(); // force decision at island 9
+			if (aOrDefault) this.curIsland = this.curIsland.baseLeft; // no branch, or going to 'a'
+			else this.curIsland = this.curIsland.baseRight; // going to 'b'
+		}	
 		return this.curIsland;
 	}
 }
@@ -56,19 +55,23 @@ class IslandNode {
 	}
 }
 
-const filledIslands = []; // array of island objects
-//array of chunks of text
+function loadText() {
+	$.get('./islandText.txt',{},function(content){
+		const tree = new Tree();
+		fillIslands(content.split('|'),tree);
+		console.log(tree.getNextIsland());
+		console.log(tree.getNextIsland());
+		console.log(tree.getNextIsland());
 
-$.get('./islandText.txt',{},function(content){
-	saveValue(content.split('|'));
-});
-function saveValue(list) {return list;}
 
-console.log(islandText.length);
-
-function fillIslands () {
-	for(let x = 0; x < 1; x++) {
-		let fieldTexts = islandText[0].split(':');
+	});
+}
+// takes in an array of text chunks, parses them into fields for each island
+function fillIslands (islandText,tree) {
+	const filledIslands = [];
+	for(let x = 0; x < 17; x++) {
+		let sanitizedText = islandText[x].replaceAll("\r\n","");
+		let fieldTexts = sanitizedText.split(':');
 		const obj = new IslandNode();
 		obj["tag"] = fieldTexts[0];
 		obj["text"] = fieldTexts[1];
@@ -79,26 +82,31 @@ function fillIslands () {
 		obj["effectType"] = fieldTexts[6];
 		filledIslands.push(obj);
 	}
+	fillTree(filledIslands,tree);
 }
 
-fillIslands()
+loadText()
 
-var tree = new Tree();
-tree.add(filledIslands[0]); //1
-tree.add(filledIslands[1]); //2a
-tree.add(filledIslands[3]); //3
-tree.add(filledIslands[4]); //4a
-tree.add(filledIslands[6]); //5
-tree.add(filledIslands[7]); //6a
-tree.add(filledIslands[9]); //7
-tree.add(filledIslands[10]); //8a
-tree.add(filledIslands[12]); //9
-tree.add(filledIslands[13]); //10a
-tree.add(filledIslands[15]); //11a
-tree.add(filledIslands[14]); //10b
-tree.add(filledIslands[16]); //11b
-tree.add(filledIslands[2],'1'); //2b
-tree.add(filledIslands[5],'3'); //4b
-tree.add(filledIslands[8],'5'); //6b
-tree.add(filledIslands[11],'7'); //8b
+function fillTree(filledIslands,tree) {
+	tree.add(filledIslands[0]); //1
+	tree.add(filledIslands[1]); //2a
+	tree.add(filledIslands[3]); //3
+	tree.add(filledIslands[4]); //4a
+	tree.add(filledIslands[6]); //5
+	tree.add(filledIslands[7]); //6a
+	tree.add(filledIslands[9]); //7
+	tree.add(filledIslands[10]); //8a
+	tree.add(filledIslands[12]); //9
+	tree.add(filledIslands[13]); //10a
+	tree.add(filledIslands[15]); //11a
+	tree.add(filledIslands[14]); //10b
+	tree.add(filledIslands[16]); //11b
+	tree.add(filledIslands[2],'1'); //2b
+	tree.add(filledIslands[5],'3'); //4b
+	tree.add(filledIslands[8],'5'); //6b
+	tree.add(filledIslands[11],'7'); //8b
+}
+
+
+
 //1, 2a, 3, 4a, 5, 6a, 7, 8a, 9, 10a, 11a, 10b, 11b, (2b,'1'), (4b,'3'), (6b, '5'), (8b,'7')
